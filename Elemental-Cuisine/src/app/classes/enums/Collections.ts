7dc9109c7a413f16df921dfc3cbad3f074d1c366
brianpariz <brian.pariz@ex-cle.com>
2020-06-23 21:32:48 -0400

    se borra codigo viejo

--- Elemental-Cuisine/src/app/classes/enums/Collections.ts
+++ Elemental-Cuisine/src/app/classes/enums/Collections.ts
@@ -4,8 +4,7 @@ export enum Collections {
     Devices = 'dispositivos',
     Tables = 'mesas',
     Products = 'productos',
-    EmployeePolls = 'encuestas_empleados',
-    ClientPolls = 'encuestas_clientes',
+    ClientPoll = 'encuesta_clientes',
     Orders = 'pedidos',
     CurrentAttention = 'servicioDeMesa',
     EmailTemplates = "email_templates"
