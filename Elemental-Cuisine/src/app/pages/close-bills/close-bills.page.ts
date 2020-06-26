29c832a1842e7645a6e3a399f97749df731ec8ed
brianpariz <brian.pariz@ex-cle.com>
2020-06-26 00:36:25 -0400

    Se completó el cierre de cuentas

---
+++ Elemental-Cuisine/src/app/pages/close-bills/close-bills.page.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { CurrentAttentionService } from 'src/app/services/currentAttention.service';
+import { Attention } from 'src/app/classes/attention';
+import { TableService } from 'src/app/services/table.service';
+import { Table } from 'src/app/classes/table';
+import { OrderService } from 'src/app/services/order.service';
+import { Status } from 'src/app/classes/enums/Status';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/classes/user';
+import { Collections } from 'src/app/classes/enums/collections';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-close-bills',
+  templateUrl: './close-bills.page.html',
+  styleUrls: ['./close-bills.page.scss'],
+})
+export class CloseBillsPage implements OnInit {
+
+  private attentions: Array<Attention>;
+  private pendingBills: Array<any>;
+
+  constructor(
+    private dataService: DataService,
+    private currentAttentionService: CurrentAttentionService,
+    private tableService: TableService,
+    private orderService: OrderService) {
+    this.pendingBills = new Array<any>();
+  }
+
+  ngOnInit() {
+    this.currentAttentionService.getAllAttentions().subscribe(attentions => {      
+      this.attentions = attentions.map(attentionsAux => {
+        let attention = attentionsAux.payload.doc.data() as Attention;
+        attention.id = attentionsAux.payload.doc.id;
+        return attention;
+      });
+
+      this.attentions.forEach(attention => {
+        if (attention.billRequested) {
+          this.tableService.getTableById(attention.tableId).then(resp => {
+            let table = Object.assign(new Table, resp.data());
+            this.pendingBills.push({ "id": attention.id, "tableId": attention.tableId, "tableNumber": "Mesa " + table.number })
+          });
+        }
+      });
+    });
+  }
+
+  closeBill(id) {
+    let pendingBill = this.pendingBills.find(x => x.id == id);
+
+    this.dataService.setStatus(Collections.Users, pendingBill.id, Status.Unattended);
+    this.dataService.setStatus(Collections.Tables, pendingBill.tableId, Status.Available);
+
+    this.orderService.deleteOrder(id);
+    this.currentAttentionService.deleteAttention(id);
+
+    this.pendingBills = this.pendingBills.filter(x => x.id != id);
+  }
+}
